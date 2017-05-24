5c09422a2141bfbf7e7aa8bedfb0abb0ef3b40d8
Aliweea <wzn40899@163.com>
2017-05-24 19:08:52 +0800

    Revert "Update AppInfoDao.java"

--- AppInfoDao.java
+++ AppInfoDao.java
@@ -1,54 +1,28 @@
 package com.mycompany.timemanagement;
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
-
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by asus on 2017/5/23.
- */
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 public class AppInfoDao {
 
-     /*
-     * 查询手机内非系统应用
+    /**
+     * 获取手机中所有的app包名集合
      * @param context
-     * @return app信息的集合
+     * @return 包名的集合
      */
-
-    public static List<AppInfo> getAllApps(Context context) {
-        List<AppInfo> appList = new ArrayList<AppInfo>();
+    public List<String> getAllApps(Context context){
+        List<String> packageNames  = new ArrayList<String>();
         PackageManager pm = context.getPackageManager();
-
-        List<PackageInfo> packages = pm.getInstalledPackages(0);
-        for(int i=0; i<packages.size(); i++) {
-            PackageInfo packageInfo = packages.get(i);
-            //确认是非系统应用
-            if ((packageInfo.applicationInfo.flags & packageInfo.applicationInfo.FLAG_SYSTEM) <= 0){
-                AppInfo tmpInfo =new AppInfo();
-                tmpInfo.appName = pm.getApplicationLabel(packageInfo.applicationInfo).toString();
-                tmpInfo.packageName = packageInfo.packageName;
-                tmpInfo.versionName = packageInfo.versionName;
-                tmpInfo.versionCode = packageInfo.versionCode;
-                tmpInfo.appIcon = pm.getApplicationIcon(packageInfo.applicationInfo);
-                appList.add(tmpInfo);
-            }
+        List<PackageInfo> infos = pm.getInstalledPackages(0);
+        for (PackageInfo info : infos) {
+            String packageName = info.packageName;
+            packageNames.add(packageName);
         }
-        return appList;
+        return packageNames;
     }
-
-
 }
 
-class AppInfo {
-    public String appName="";
-    public String packageName="";
-    public String versionName="";
-    public int versionCode=0;
-    public Drawable appIcon=null;
-}
